5b753a40b61303b15d6461c32be713e7a1bd2fe8
Ahorsode Benjamin Delali <ahorsodebenjamindelali@gmail.com>
2026-03-13 23:33:57 +0000

    interface update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -17,89 +17,217 @@ namespace liveVideos
 
         private void InitializeComponent()
         {
-            this.txtRoomName = new System.Windows.Forms.TextBox();
+            this.topPanel = new System.Windows.Forms.Panel();
             this.btnJoin = new System.Windows.Forms.Button();
+            this.txtRoomName = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.mainContainer = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
             this.lstChat = new System.Windows.Forms.ListBox();
+            this.chatInputPanel = new System.Windows.Forms.Panel();
             this.txtMessage = new System.Windows.Forms.TextBox();
             this.btnSend = new System.Windows.Forms.Button();
             
+            this.topPanel.SuspendLayout();
+            this.mainContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.chatInputPanel.SuspendLayout();
             this.SuspendLayout();
-            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(1200, 700);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "LiveClass Pro - Video Conferencing";
+            // 
+            // topPanel
+            // 
+            this.topPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.topPanel.Controls.Add(this.btnJoin);
+            this.topPanel.Controls.Add(this.txtRoomName);
+            this.topPanel.Controls.Add(this.lblTitle);
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(1200, 70);
+            this.topPanel.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI Semilight", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.Location = new System.Drawing.Point(20, 16);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(166, 38);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "LiveClass Pro";
+            // 
             // txtRoomName
-            this.txtRoomName.Location = new System.Drawing.Point(12, 12);
+            // 
+            this.txtRoomName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRoomName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.txtRoomName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRoomName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtRoomName.ForeColor = System.Drawing.Color.White;
+            this.txtRoomName.Location = new System.Drawing.Point(820, 19);
             this.txtRoomName.Name = "txtRoomName";
-            this.txtRoomName.Size = new System.Drawing.Size(200, 23);
-            this.txtRoomName.TabIndex = 0;
-            this.txtRoomName.PlaceholderText = "Enter Room Name";
-            
+            this.txtRoomName.PlaceholderText = "   Enter Room Name...";
+            this.txtRoomName.Size = new System.Drawing.Size(240, 34);
+            this.txtRoomName.TabIndex = 1;
+            // 
             // btnJoin
-            this.btnJoin.Location = new System.Drawing.Point(218, 12);
+            // 
+            this.btnJoin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnJoin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnJoin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnJoin.FlatAppearance.BorderSize = 0;
+            this.btnJoin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnJoin.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnJoin.Location = new System.Drawing.Point(1075, 18);
             this.btnJoin.Name = "btnJoin";
-            this.btnJoin.Size = new System.Drawing.Size(75, 23);
-            this.btnJoin.TabIndex = 1;
-            this.btnJoin.Text = "Join";
-            this.btnJoin.UseVisualStyleBackColor = true;
+            this.btnJoin.Size = new System.Drawing.Size(100, 36);
+            this.btnJoin.TabIndex = 2;
+            this.btnJoin.Text = "JOIN";
+            this.btnJoin.UseVisualStyleBackColor = false;
             this.btnJoin.Click += new System.EventHandler(this.btnJoin_Click);
-            
+            // 
+            // mainContainer
+            // 
+            this.mainContainer.Controls.Add(this.splitContainer1);
+            this.mainContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainContainer.Location = new System.Drawing.Point(0, 70);
+            this.mainContainer.Name = "mainContainer";
+            this.mainContainer.Padding = new System.Windows.Forms.Padding(10);
+            this.mainContainer.Size = new System.Drawing.Size(1200, 630);
+            this.mainContainer.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(10, 10);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.webView);
+            this.splitContainer1.Panel1.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lstChat);
+            this.splitContainer1.Panel2.Controls.Add(this.chatInputPanel);
+            this.splitContainer1.Panel2.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.splitContainer1.Size = new System.Drawing.Size(1180, 610);
+            this.splitContainer1.SplitterDistance = 850;
+            this.splitContainer1.SplitterWidth = 8;
+            this.splitContainer1.TabIndex = 0;
+            // 
             // webView
+            // 
             this.webView.AllowExternalDrop = true;
             this.webView.CreationProperties = null;
-            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
-            this.webView.Location = new System.Drawing.Point(12, 41);
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.webView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView.Location = new System.Drawing.Point(0, 0);
             this.webView.Name = "webView";
-            this.webView.Size = new System.Drawing.Size(560, 397);
-            this.webView.TabIndex = 2;
+            this.webView.Size = new System.Drawing.Size(845, 610);
+            this.webView.TabIndex = 0;
             this.webView.ZoomFactor = 1D;
-            
-            // lstChat
-            this.lstChat.FormattingEnabled = true;
-            this.lstChat.ItemHeight = 15;
-            this.lstChat.Location = new System.Drawing.Point(587, 41);
-            this.lstChat.Name = "lstChat";
-            this.lstChat.Size = new System.Drawing.Size(201, 364);
-            this.lstChat.TabIndex = 3;
-            
+            // 
+            // chatInputPanel
+            // 
+            this.chatInputPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.chatInputPanel.Controls.Add(this.txtMessage);
+            this.chatInputPanel.Controls.Add(this.btnSend);
+            this.chatInputPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.chatInputPanel.Location = new System.Drawing.Point(5, 540);
+            this.chatInputPanel.Name = "chatInputPanel";
+            this.chatInputPanel.Padding = new System.Windows.Forms.Padding(10);
+            this.chatInputPanel.Size = new System.Drawing.Size(317, 70);
+            this.chatInputPanel.TabIndex = 1;
+            // 
             // txtMessage
-            this.txtMessage.Location = new System.Drawing.Point(587, 415);
+            // 
+            this.txtMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.txtMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMessage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtMessage.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtMessage.ForeColor = System.Drawing.Color.White;
+            this.txtMessage.Location = new System.Drawing.Point(10, 10);
             this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(120, 23);
-            this.txtMessage.TabIndex = 4;
-            this.txtMessage.PlaceholderText = "Type message";
-            
+            this.txtMessage.PlaceholderText = " Type a message...";
+            this.txtMessage.Size = new System.Drawing.Size(217, 34);
+            this.txtMessage.TabIndex = 0;
+            // 
             // btnSend
-            this.btnSend.Location = new System.Drawing.Point(713, 415);
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnSend.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSend.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnSend.FlatAppearance.BorderSize = 0;
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSend.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSend.Location = new System.Drawing.Point(227, 10);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 23);
-            this.btnSend.TabIndex = 5;
-            this.btnSend.Text = "Send";
-            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Size = new System.Drawing.Size(80, 50);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "SEND";
+            this.btnSend.UseVisualStyleBackColor = false;
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lstChat
+            // 
+            this.lstChat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.lstChat.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstChat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstChat.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lstChat.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.lstChat.FormattingEnabled = true;
+            this.lstChat.ItemHeight = 25;
+            this.lstChat.Location = new System.Drawing.Point(5, 0);
+            this.lstChat.Name = "lstChat";
+            this.lstChat.Size = new System.Drawing.Size(317, 540);
+            this.lstChat.TabIndex = 0;
             
-            // Form1
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnSend);
-            this.Controls.Add(this.txtMessage);
-            this.Controls.Add(this.lstChat);
-            this.Controls.Add(this.webView);
-            this.Controls.Add(this.btnJoin);
-            this.Controls.Add(this.txtRoomName);
-            this.Name = "Form1";
-            this.Text = "Live Video Conferencing";
+            this.Controls.Add(this.mainContainer);
+            this.Controls.Add(this.topPanel);
+            
+            this.topPanel.ResumeLayout(false);
+            this.topPanel.PerformLayout();
+            this.mainContainer.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.chatInputPanel.ResumeLayout(false);
+            this.chatInputPanel.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.TextBox txtRoomName;
         private System.Windows.Forms.Button btnJoin;
+        
+        private System.Windows.Forms.Panel mainContainer;
+        private System.Windows.Forms.SplitContainer splitContainer1;
         private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        
         private System.Windows.Forms.ListBox lstChat;
+        private System.Windows.Forms.Panel chatInputPanel;
         private System.Windows.Forms.TextBox txtMessage;
         private System.Windows.Forms.Button btnSend;
     }
--- obj/Debug/net10.0-windows/liveVideos.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/liveVideos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("liveVideos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+874718feb4fd1247fb6052922456f304f5b478b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("liveVideos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("liveVideos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
